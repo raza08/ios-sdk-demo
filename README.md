023a163e69cadfce30b785d5ae17176bd9710184
dmytro.khl <dmytro.khludkov@vgs.io>
2020-01-23 17:36:48 +0200

    Create demo app

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
-# ios-sdk-demo
+## VGS Collect iOS SDK Demo
+
+This examples shows how easily you can integrate <a href="https://github.com/verygoodsecurity/vgs-collect-android">VGS Collect iOS SDK</a> 
+into your application and secure sensitive data with us.
+
+<p align="center">
+    <img src="./vgs-collect-ios-demo-gif-low.gif" width="250">
+</p>
+
+## How to run it?
+
+### Requirements
+
+- Installed <a href="https://apps.apple.com/us/app/xcode/id497799835?mt=12" target="_blank">Xcode</a>
+- Installed <a href="https://guides.cocoapods.org/using/getting-started.html#installation" target="_blank">CocoaPods</a>
+- Organization with <a href="https://www.verygoodsecurity.com/">VGS</a>
+
+
+#### Step 1
+
+Go to your <a href="https://dashboard.verygoodsecurity.com/" target="_blank">VGS organization</a> and establish <a href="https://www.verygoodsecurity.com/docs/getting-started/quick-integration#securing-inbound-connection" target="_blank">Inbound connection</a>. 
+
+#### Step 2
+
+Clone demo application repository.
+
+``https://github.com/verygoodsecurity/ios-sdk-demo.git``
+
+#### Step 3
+
+Install demoapp pods.
+
+Open Terminal and change working directory to `demoapp` inside `ios-sdk-demo` folder:
+`$ cd ~/demoapp`
+
+Install pods:
+`pod install`
+
+
+#### Step 4
+
+In `demoapp` folder find and open demoapp`.xcworkspace` file.
+In the app go to `ViewController.swift` file, find the line
+`var vgsForm = VGSCollect(id: "VaultId", environment: .sandbox)`
+and replace `VaultId` with your organization
+ <a href="https://www.verygoodsecurity.com/docs/terminology/nomenclature#vault" target="_blank">vault id</a>. 
+ 
+### Step 5 
+
+Run the application and submit the form. 
+Then go to the Logs tab on <a href="http://dashboard.verygoodsecurity.com" target="_blank">Dashboard</a>, find request and secure a payload. 
+Instruction for this step you can find <a href="https://www.verygoodsecurity.com/docs/getting-started/quick-integration#securing-inbound-connection" target="_blank">here</a>.
+
+### Useful links
+
+- <a href="https://www.verygoodsecurity.com/docs/vgs-collect/ios-sdk" target="_blank">Documentation</a> 
+- <a href="https://github.com/verygoodsecurity/vgs-collect-ios" target="_blank">Repo</a> 
+- <a href="http://cocoapods.org/pods/VGSCollectSDK" target="_blank">CocoaPods</a> 
+
+
---
+++ demoapp/demoapp/ViewController.swift
@@ -0,0 +1,273 @@
+//
+//  ViewController.swift
+//  demoapp
+//
+//  Created by Vitalii Obertynskyi on 8/14/19.
+//  Copyright © 2019 Vitalii Obertynskyi. All rights reserved.
+//
+
+import UIKit
+import VGSCollectSDK
+import SnapKit
+
+class ViewController: UIViewController {
+    var consoleLabel: UILabel!
+    var consoleStatusLabel: UILabel!
+    
+    var consoleMessage: String = "" {
+        didSet {
+            consoleLabel.text = consoleMessage
+        }
+    }
+    // Collector vgs
+    var vgsForm = VGSCollect(id: "VaultId", environment: .sandbox)
+    
+    // VGS UI Elements
+    // initialase you cardNumber like a VGSCardTextField class
+    var cardNumber = VGSCardTextField()
+    var expCardDate = VGSTextField()
+    var cvcCardNum = VGSTextField()
+    var cardHolderName = UITextField(frame: .zero)
+    
+    // the Send data Button
+    var sendButton = UIButton()
+    
+    // MARK: - Life cycle methods
+    override func loadView() {
+        super.loadView()
+        setupUI()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // check if device is jailbroken
+        if VGSCollect.isJailbroken() {
+            print("Device is Jailbroken")
+        }
+        
+        // set custom headers
+        vgsForm.customHeaders = [
+            "my custome header": "some custom data"
+        ]
+        
+        // Observing text fields
+        vgsForm.observeStates = { [weak self] form in
+            
+            self?.consoleMessage = ""
+            self?.consoleStatusLabel.text = "STATE"
+
+            form.forEach({ textField in
+                self?.consoleMessage.append(textField.state.description)
+                self?.consoleMessage.append("\n")
+            })
+        }
+        
+        // if you want to set your card brand icon then use this closure
+//        cardNumber.cardsIconSource = { cardType in
+//            switch cardType {
+//            case .mastercard:
+//                return UIImage(named: "your mastercard icon")
+//            case .visa:
+//                return UIImage(named: "your visa icon")
+//
+//            default:
+//                return nil
+//            }
+//        }
+        setupElements()
+    }
+    
+    // MARK: - Init UI
+    private func setupUI() {
+        // title
+        let titleLabel = UILabel()
+        titleLabel.text = "Collecting credit card data"
+        titleLabel.textAlignment = .center
+        titleLabel.font = UIFont.systemFont(ofSize: 22)
+        titleLabel.textColor = .black
+        view.addSubview(titleLabel)
+        titleLabel.snp.makeConstraints { make in
+            make.left.equalTo(25)
+            make.height.equalTo(30)
+            make.centerX.equalToSuperview()
+            make.top.equalTo(55)
+        }
+
+        view.addSubview(cardHolderName)
+        cardHolderName.snp.makeConstraints { make in
+            make.left.equalTo(25)
+            make.height.equalTo(45)
+            make.centerX.equalToSuperview()
+            make.top.equalTo(120)
+        }
+        
+        // setup card number text field
+        view.addSubview(cardNumber)
+        cardNumber.snp.makeConstraints { make in
+            make.left.equalTo(25)
+            make.height.equalTo(45)
+            make.centerX.equalToSuperview()
+            make.top.equalTo(cardHolderName.snp.bottom).offset(10)
+        }
+        // setup expiration card date
+        view.addSubview(expCardDate)
+        expCardDate.snp.makeConstraints { make in
+            make.left.equalTo(25)
+            make.height.equalTo(45)
+            make.centerX.equalToSuperview()
+            make.top.equalTo(cardNumber.snp.bottom).offset(10)
+        }
+        
+        // setup CVC card number
+        view.addSubview(cvcCardNum)
+        cvcCardNum.snp.makeConstraints { make in
+            make.left.equalTo(25)
+            make.height.equalTo(45)
+            make.centerX.equalToSuperview()
+            make.top.equalTo(expCardDate.snp.bottom).offset(10)
+        }
+        
+        // init send button
+        sendButton.setTitle("Submit", for: .normal)
+        sendButton.backgroundColor = UIColor(red: 0.337, green: 0.761, blue: 0.333, alpha: 1.00)
+        sendButton.layer.cornerRadius = 6
+        sendButton.clipsToBounds = true
+        view.addSubview(sendButton)
+        sendButton.snp.makeConstraints { make in
+            make.left.equalTo(25)
+            make.height.equalTo(55)
+            make.centerX.equalToSuperview()
+            make.top.equalTo(cvcCardNum.snp.bottom).offset(35)
+        }
+        
+        // setup console views
+        let separastor = UIView()
+        separastor.backgroundColor = .lightGray
+        view.addSubview(separastor)
+        separastor.snp.makeConstraints { make in
+            make.top.equalTo(sendButton.snp.bottom).offset(35)
+            make.left.right.equalTo(0)
+            make.height.equalTo(1)
+        }
+        
+        let consoleBackgroundView = UIView()
+        consoleBackgroundView.backgroundColor = UIColor(red: 240/255, green: 240/255, blue: 240/255, alpha: 1)
+        view.addSubview(consoleBackgroundView)
+        consoleBackgroundView.snp.makeConstraints { make in
+            make.top.equalTo(separastor.snp.bottom)
+            make.left.right.equalTo(0)
+            make.bottom.equalToSuperview().priority(250)
+        }
+        
+        consoleStatusLabel = UILabel(frame: .zero)
+        consoleStatusLabel.text = "Waiting for data..."
+        consoleStatusLabel.numberOfLines = 0
+        consoleStatusLabel.textAlignment = .center
+        consoleStatusLabel.textColor = .darkGray
+        consoleStatusLabel.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        consoleStatusLabel.backgroundColor = .white
+        consoleBackgroundView.addSubview(consoleStatusLabel)
+        consoleStatusLabel.snp.makeConstraints { make in
+            make.top.left.right.equalTo(0)
+            make.height.equalTo(40)
+        }
+        
+        consoleLabel = UILabel(frame: .zero)
+        consoleLabel.text = ""
+        consoleLabel.textAlignment = .left
+        consoleLabel.numberOfLines = 0
+        consoleLabel.contentMode = .topLeft
+        consoleLabel.textColor = .black
+        consoleBackgroundView.addSubview(consoleLabel)
+        
+        consoleLabel.snp.makeConstraints { make in
+            make.top.equalTo(consoleStatusLabel.snp.bottom).offset(8)
+            make.left.right.equalTo(15)
+        }
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        consoleLabel.addGestureRecognizer(tapGesture)
+        consoleLabel.isUserInteractionEnabled = true
+    }
+    
+    @objc
+    func hideKeyboard() {
+        view.endEditing(true)
+    }
+    
+    private func setupElements() {
+        
+        let textColor = UIColor.label
+        let textFont = UIFont.systemFont(ofSize: 22)
+        let padding = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
+        
+        let cardConfiguration = VGSConfiguration(collector: vgsForm, fieldName: "card_number")
+        cardConfiguration.isRequired = true
+        cardConfiguration.type = .cardNumber
+        
+        cardNumber.configuration = cardConfiguration
+        cardNumber.textColor = textColor
+        cardNumber.font = textFont
+        cardNumber.padding = padding
+        cardNumber.placeholder = "Card Number"
+        cardNumber.textAlignment = .natural
+        
+        let expDateConfiguration = VGSConfiguration(collector: vgsForm, fieldName: "card_expirationDate")
+        expDateConfiguration.isRequired = true
+        expDateConfiguration.type = .expDate
+        
+        expCardDate.configuration = expDateConfiguration
+        expCardDate.textColor = textColor
+        expCardDate.font = textFont
+        expCardDate.padding = padding
+        expCardDate.placeholder = "MM/YY"
+        
+        let cvcConfiguration = VGSConfiguration(collector: vgsForm, fieldName: "card_cvc")
+        cvcConfiguration.isRequired = true
+        cvcConfiguration.type = .cvc
+        
+        cvcCardNum.configuration = cvcConfiguration
+        cvcCardNum.textColor = textColor
+        cvcCardNum.font = textFont
+        cvcCardNum.padding = padding
+        cvcCardNum.placeholder = "CVC"
+        
+        cardHolderName.layer.borderWidth = 1
+        cardHolderName.layer.borderColor = UIColor.lightGray.cgColor
+        cardHolderName.layer.cornerRadius = 4
+        cardHolderName.placeholder = "Name"
+        cardHolderName.font = textFont
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 8, height: cardHolderName.frame.height))
+        cardHolderName.leftView = paddingView
+        cardHolderName.leftViewMode = .always
+        
+        // Add target for send button
+        sendButton.addTarget(self, action: #selector(sendData(_:)), for: .touchUpInside)
+    }
+}
+
+// MARK: - send/receive data
+extension ViewController {
+    @objc
+    func sendData(_ sender: UIButton) {
+        consoleLabel.text = "Processing..."
+        
+        // hide kayboard
+        hideKeyboard()
+        
+        // send extra data
+        var extraData = [String: Any]()
+        extraData["cardHolderName"] = cardHolderName.text
+        
+        // send data
+        vgsForm.submit(path: "post", extraData: extraData, completion: { [weak self] (json, error) in
+            self?.consoleStatusLabel.text = "RESPONSE"
+            if error == nil, let json = json {
+                self?.consoleLabel.text = (String(data: try! JSONSerialization.data(withJSONObject: json, options: .prettyPrinted), encoding: .utf8)!)
+            } else {
+                self?.consoleLabel.text = "Something went wrong!"
+                print("Error: \(String(describing: error?.localizedDescription))")
+            }
+        })
+    }
+}
